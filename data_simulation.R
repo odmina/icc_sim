177541f09b1f41897cf785094f0755e8b4ead3b0
Gosia Basińska <31844953+odmina@users.noreply.github.com>
2023-01-12 20:51:05 +0100

    code clarifications

--- data_simulation.R
+++ data_simulation.R
@@ -1,5 +1,3 @@
-# WITH DECREASING INTRACLASS CORRELATION AUC GOES DOWN!!!!!!
-
 # %% SIMULATION ASSUMPTIONS
 # Based on Rudd's fluid vulnerability theory (2006),
 # which states that:
@@ -34,65 +32,77 @@ tv_days <- seq.int(1, n_days, 1)
 N <- 20000
 population <- data.table(id = seq.int(1, N, 1))
 
-# %% BASELINE RISK
-# is represented by a random effect (it would be a random intercept in a mixed model).
-# Normally distributed.
-base_risk_SD <- 1
-population$base_risk <- rnorm(N, mean = 0, sd = base_risk_SD)
+# %% FUNCTION THAT DECOMPOSES TOTAL VARIANCE TO TRUE SCORE VARIANCE AND BY ICC
+# Calculation based on Model 1 form 
+#    Liljequist, D., Elfving, B., & Skavberg Roaldsen, K. (2019). 
+#       Intraclass correlation – A discussion and demonstration of basic features. 
+#       PLoS ONE, 14(7), e0219854. https://doi.org/10.1371/journal.pone.0219854
+# Uses the following formula  ICC = var_r / (var_r + var_v) where 
+#   var_r is the variance of means per subject (centered around sample mean)
+#   var_v is the variance of scores whitin subjects
+# Returns a list with var_r and var_v
+get_disp_from_ICC <- function(sd_total = 1, ICC = 0.5) {
+    var_r = var_total * ICC
+    var_v = var_r * (1 - ICC) / ICC
+    return(list(var_r = var_r, 
+                var_v = var_v))  
+}
 
 # %% PREDICTOR THAT FLUCTUATES DAILY
 # Based on Bernstein et. al. 2021 I assume that:
 #   - mean levels of the predictor differ;
 #   - SDs also vary from subject to subject, but for simplicity, 
 #       SDs are held constant (will not affect the results).
-# Pedictor levels are calculated based on Model 1 form 
-#    Liljequist, D., Elfving, B., & Skavberg Roaldsen, K. (2019). 
-#       Intraclass correlation – A discussion and demonstration of basic features. 
-#       PLoS ONE, 14(7), e0219854. https://doi.org/10.1371/journal.pone.0219854
 # pred_ICC represents intraclass corelation (ICC).
-# Given ICC and true score variance (pred_ri_sd), 
-#   intra-class variance is calculated (pred_ic_SD ** 2).
+# pred_var_total represents total variance of the predictior
 pred_ICC <- 0.5
-pred_ri_sd <- 1
-pred_vij_sd <- sqrt((pred_ri_sd ** 2 * (1 - pred_ICC)) / pred_ICC)
-population$pred_ri <- rnorm(N, 0, pred_ri_sd)
+pred_var_total <- 1
+pred_vars <- get_var_from_ICC(pred_var_total, pred_ICC)
+pred_sd_r <- sqrt(pred_vars$var_r)
+pred_sd_v <- sqrt(pred_vars$var_v)
+
+# %% PER SUBJECT RISK & DAILY RANDOM RISK FLUCTUATION
+# Noise per subject 
+#   (variance of random effect in not caused by the variance of the mean predictor level). 
+# Daily random risk fluctuations are error term for daily measurements.
+# To account for their relative relevance ICC is also used.
+noise_ICC <- 0.5
+noise_var_total <- 1
+noise_vars <- get_var_from_ICC(noise_var_total, noise_ICC)
+noise_sd_r <- sqrt(noise_vars$var_r)
+noise_sd_v <- sqrt(noise_vars$var_v)
+
+# %% ADD NOISE AND PREDICTOR VARIABLES ON SUBJECT LEVEL
+population$noise_r <- rnorm(N, mean = 0, sd = noise_sd_r)
+population$pred_r <- rnorm(N, mean = 0, sd = pred_sd_r)
+
+# %% ADD DATA ON DAYS LEVEL
 rep_id_vec <- sort(rep(seq.int(1, N, 1), n_days)) #repeats each id n_days times
 pop_days <- population[rep_id_vec, ]
 pop_days$day <- rep(tv_days, N)
-pop_days$predictor <- pop_days$pred_ri + rnorm(n = n_days * N, sd = pred_vij_sd, mean = 0)
-#pop_days$predictor <- unlist(lapply(s_sample$pred_mean, rnorm, n = n_days, sd = pred_ic_sd))
-
-# %% DAILY RANDOM RISK FLUCTUATION
-# results from day to day risk and protective factors with varying hazard periods.
-# Also normally distributed, same variance as baseline risk - it does not have to be the case
-#   but on the other hand it should not affect the effect of simulation.
-noise_SD <- 1
-pop_days$noise <- rnorm(N * n_days, mean = 0, sd = noise_SD)
+pop_days$predictor <- pop_days$pred_r + rnorm(n = n_days * N, sd = pred_sd_v, mean = 0)
+pop_days$noise <- pop_days$noise_r + rnorm(n = n_days * N, sd = noise_sd_v, mean = 0)
 
 # %% FINAL SA RISK CALCULATION
-pop_days$sa_risk <- pop_days$base_risk + pop_days$predictor + pop_days$noise
+variance_scaling = 0.2
+pop_days$sa_risk <- variance_scaling * pop_days$predictor + pop_days$noise
 
 # %% BINARY OUTCOME
-# Two alternatives:
-#   - general population with 0,4% SA twelve-month prevalence (Borges et. al., 2010);
-#   - clinical cohort with higher twelve-month prevalence of SA.
-# I simulate clinical cohort.
-SA_incidence_year <- 0.06
-SA_incidence_day <- SA_incidence_year / 365
+SA_incidence_day <- 0.0014
 cutoff <- quantile(pop_days$sa_risk, probs = 1 - SA_incidence_day)
 pop_days$sa <- ifelse(pop_days$sa_risk > cutoff, 1, 0)
 
 # %% SAMPLE
-sample_n <- 1000
+sample_n <- 200
 sample_ids <- sample(1:N, size = sample_n, replace = FALSE)
 sample_days <- pop_days[id %in% sample_ids]
 
-# %% checks - to be removed
+# %% checks 
 pop_days %>% 
     group_by(id) %>%
     summarise(attempts = sum(sa)) %>%
     group_by(attempts) %>%
-    summarise(n = n())
+    summarise(n = n(), proc = n / N)
 sample_days %>% 
     group_by(id) %>%
     summarise(attempts = sum(sa)) %>%
@@ -107,13 +117,12 @@ m_lme <- glmer(sa ~ predictor + (1|id), data = sample_days, family = "binomial")
 # first make predictions based on variable predictor
 # do not include intercepts (it is achieved by re.form = ~0)
 pred_days <- pop_days[!(id %in% sample_ids)]
-pred_days$m_lme_pred <- predict(m_lme, newdata = pred_days, re.form = ~0, type = "response")
+pred_days$m_lme_pred <- predict(m_lme, 
+    newdata = pred_days, 
+    re.form = ~0, 
+    type = "response")
 print("BASE")
 (my_roc <- roc(response = pred_days$sa, predictor = pred_days$m_lme_pred))
-
-# %% ICC
-print("ICC")
-(icc <- lmer(predictor ~ day + (1|id), data = sample_days))
 
 # %% Simple model with one measurement and follow-up 
 # I start with sampling one day and then check whether a SA occured during follow-up
@@ -121,7 +130,7 @@ print("ICC")
 # 1. get predictor level
 predictor <- pop_days[pop_days$day == 365, c("id", "predictor")]
 # 2. get info on SA during followup
-sa_followup <- pop_days[ , 
+sa_followup <- pop_days[ ,  
     .(sa = any(sa == 1 & day > 365)), 
     by = id]
 # 3. join predictor with info about SA
