bac9b9397f8e800d42697725d6b783975581428d
Gosia Basińska <31844953+odmina@users.noreply.github.com>
2023-02-16 21:16:43 +0100

    code to run simulation repeatedly

---
+++ run.R
@@ -0,0 +1,78 @@
+# %% LIBRARIES
+library(parallel)
+
+# %% CREATE A DIRECTORY TO STORE SIMULATION RESULTS
+dir_name <- paste0("sim_results_", Sys.Date(), "_", format(Sys.time(), "%H%M%S"))
+dir.create(dir_name)
+
+# %% SET SIMULATION PARAMETERS
+N <- 20000
+sample_sizes <- c(100, 200, 500, 1000)
+pred_ICCs <- c(0.3, 0.5, 0.7)
+noise_ICCs <- c(0.5)
+gtm_betas <- c(0.2, 0.3, 0.4, 0.5)
+SA_incidences_days <- 0.01
+
+# %% EXPAND PARAMETERS TO A GRID
+params <- expand.grid(N,
+    sample_sizes,
+    pred_ICCs,
+    noise_ICCs,
+    gtm_betas,
+    SA_incidences_days)
+names(params) <- c("N",
+    "sample_sizes",
+    "pred_ICCs",
+    "noise_ICCs",
+    "gtm_betas",
+    "SA_incidences_days")
+
+# %% DETECT CORES
+n_cores <- detectCores()
+
+# %% HOW MANY TIMES REPEAT EACH COMBINATION OF PARAMS?
+runs <- 3
+
+# %% RUN PARALLELIZED in a loop
+for (i in seq_len(runs)) {
+    # set cluster
+    cl <- makeCluster(n_cores)
+    # load libraries to workers
+    clusterEvalQ(cl, {
+            library(dplyr)
+            library(data.table)
+            library(lme4)
+            library(pROC)
+            })
+    # set working dir for each worker
+    clusterExport(cl, "wd")
+    wd <- getwd()
+    # load helper functions for each worker
+    clusterEvalQ(cl, {
+            file_sim_data <- paste0(wd, "/fun/sim_data.R")
+            source(file_sim_data)
+            file_mean_level <- paste0(wd, "/fun/mean_level.R")
+            source(file_mean_level)
+            file_get_vars <- paste0(wd, "/fun/get_vars_from_icc.R")
+            source(file_get_vars)
+        })
+
+    #simulate data
+    sim_results <- clusterMap(cl, sim_data,
+    N = mini_params[["N"]],
+    sample_size = mini_params[["sample_sizes"]],
+    pred_ICC = mini_params[["pred_ICCs"]],
+    gtm_beta = mini_params[["gtm_betas"]],
+    SA_incidence_day = mini_params[["SA_incidences_days"]],    
+    RECYCLE = FALSE,
+    SIMPLIFY = FALSE, 
+    USE.NAMES = TRUE)
+    
+    stopCluster(cl)
+
+    # save data to csv
+    sim_results_df <- do.call("rbind", sim_results)
+    results_file <- sprintf("results%04d.csv", i)
+    write.csv(sim_results_df, paste0(wd,"/", dir_name, "/", results_file))
+}
+
